512c6d4d4b40b64e9ff291ec155f2aac8cb5dc9f
smit-raichura <smit.raichura01@gmail.com>
2025-02-25 04:02:38 -0500

    send corelation id

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 import socket  # noqa: F401
 
 
+def createMessage(corelationId, messageBody):
+    corelationId_Bytes = corelationId.to_bytes(4, byteorder = "big")
+    messageBody_Bytes = messageBody.to_bytes(4, byteorder = "big")
+    return messageBody_Bytes + corelationId_Bytes
+
+def handleClient(client):
+    client.sendall(createMessage(7, 1))
+    client.close()
+    pass
+
+
+
 def main():
     # You can use print statements as follows for debugging,
     # they'll be visible when running tests.
@@ -9,7 +21,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    while True:
+        client, addr = server.accept()
+        
+         
 
 
 if __name__ == "__main__":
